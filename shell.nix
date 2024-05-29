92c2154aaad8334f0b4f57cf51b0f75973140102
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2024-05-28 22:21:37 -0400

    Adjusts shell env and add readme

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
-  pkgs = import nixpkgs { config = {allowUnfree = true;}; overlays = []; };
+  nixpkgs = import <nixpkgs>{ config = {allowUnfree = true;}; overlays = []; }   ; #fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = nixpkgs.pkgs; #import nixpkgs { config = {allowUnfree = true;}; overlays = []; };
   nixpkgs_unstable = fetchTarball "https://github.com/NixOS/nixpkgs/archive/69ee1d82f1fa4c70a3dc9a64111e7eef3b8e4527.tar.gz";
   pkgs_unstable = import nixpkgs_unstable { config = {allowUnfree = true;}; overlays = []; };
   nixpkgs-python = import (fetchTarball "https://github.com/cachix/nixpkgs-python/archive/refs/heads/main.zip");
